2c84db1f9a4535a61b2151eef604436a8a3eb608
BartekJasek <bartek.jasek5@gmail.com>
2023-04-06 22:56:26 +0200

    test_modelscorrects

--- korkipl/korki/tests/test_forms.py
+++ korkipl/korki/tests/test_forms.py
@@ -4,21 +4,21 @@ from korki.forms import AddCityForm, TutorForm, PublicationForm, SubjectForm
 
 
 @pytest.fixture
-def addcityform():
+def test_addcityform():
     form = AddCityForm(data={"city": "Kielce", "postocode": "25-134"})
 
 
 @pytest.fixture
-def tutorform():
+def test_tutorform():
     form2 = TutorForm(data={"user": "testuser", "experience": "Uczeń Coderslab", "phone": "+48510145370",
                            "facebook_url": "https://github.com/BartekJasek/Korki.pl-BackEnd-Repo!"})
 
 
 @pytest.fixture
-def subjectform():
+def test_subjectform():
     form3 = SubjectForm(data={"subject": "informatyka"})
 
 
 @pytest.fixture
-def publicationform():
+def test_publicationform():
     form4 = PublicationForm(data={"price": "120", "subject": "matematyka", "tutor": "testuser", "city": "Kielce"})
--- korkipl/korki/tests/test_models.py
+++ korkipl/korki/tests/test_models.py
@@ -5,22 +5,31 @@ from korki.models import Publications, Tutor, Subject, City
 
 
 @pytest.mark.django_db
-def user_create():
-    User.objects.create_user(username='testuser', email='test@test.com', password1='T3stow3hasl!',
-                             password2='T3stow3hasl!', first_name='User', last_name='Test')
+def test_user_create():
+    User.objects.create_user(username='testuser', email='test@test.com', first_name='User', last_name='Test')
 
 
 @pytest.mark.django_db
-def tutor_create():
-    Tutor.objects.create(user='testuser', experience='Uczeń Coderslab', phone='+48510145370',
+def test_tutor_create():
+    testuser = User.objects.create(username='test', email='test@test.pl', first_name='test', last_name='test')
+    Tutor.objects.create(user=testuser, experience='Uczeń Coderslab', phone='+48510145370',
                          facebook_url='https://github.com/BartekJasek/Korki.pl-BackEnd-Repo!')
 
 
 @pytest.mark.django_db
-def subject_create():
+def test_subject_create():
     Subject.objects.create(subject='matematyka')
 
 
 @pytest.mark.django_db
-def city_create():
+def test_city_create():
     City.objects.create(city='Kielce', postcode='25-004')
+
+
+@pytest.mark.django_db
+def test_publication_create():
+    testuser = User.objects.create(username='test', email='test@test.pl', first_name='test', last_name='test')
+    testcity = City.objects.create(city='Kielce', postcode='25-004')
+    testsubject = Subject.objects.create(subject='biologia')
+    publication = Publications.objects.create(price='120', tutor=testuser, city=testcity)
+    publication.subject.set(testsubject)
