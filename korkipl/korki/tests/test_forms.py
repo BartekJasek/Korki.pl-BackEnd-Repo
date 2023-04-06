55ac8fc0f5ee506eb3aa7d17ea4b3e101b2b2726
BartekJasek <bartek.jasek5@gmail.com>
2023-04-06 19:46:24 +0200

    tests

--- korkipl/korki/tests/test_forms.py
+++ korkipl/korki/tests/test_forms.py
@@ -1,19 +1,24 @@
-from django.test import TestCase
+import pytest
 
 from korki.forms import AddCityForm, TutorForm, PublicationForm, SubjectForm
 
+
+@pytest.fixture
 def addcityform():
     form = AddCityForm(data={"city": "Kielce", "postocode": "25-134"})
 
 
+@pytest.fixture
 def tutorform():
     form2 = TutorForm(data={"user": "testuser", "experience": "Uczeń Coderslab", "phone": "+48510145370",
                            "facebook_url": "https://github.com/BartekJasek/Korki.pl-BackEnd-Repo!"})
 
 
+@pytest.fixture
 def subjectform():
     form3 = SubjectForm(data={"subject": "informatyka"})
 
 
+@pytest.fixture
 def publicationform():
     form4 = PublicationForm(data={"price": "120", "subject": "matematyka", "tutor": "testuser", "city": "Kielce"})
